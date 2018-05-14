3eb5431948be543666e747430e508ddbe69baa75
Alone <715213471@qq.com>
2018-05-14 11:50:36 +0800

    style(front): change icon

--- front/src/views/login.vue
+++ front/src/views/login.vue
@@ -43,7 +43,7 @@ export default {
     data () {
         return {
             form: {
-                userName: 'iview_admin',
+                userName: 'admin',
                 password: ''
             },
             rules: {
@@ -63,11 +63,6 @@ export default {
                     Cookies.set('user', this.form.userName);
                     Cookies.set('password', this.form.password);
                     this.$store.commit('setAvator', 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3448484253,3685836170&fm=27&gp=0.jpg');
-                    if (this.form.userName === 'iview_admin') {
-                        Cookies.set('access', 0);
-                    } else {
-                        Cookies.set('access', 1);
-                    }
                     this.$router.push({
                         name: 'home_index'
                     });
